47b0ca0594769008fffd1f6ccc0d847ddb0adc1c
Vansh Gupta <vgvansh25@gmail.com>
2020-08-23 20:08:42 +0530

    Add files via upload

--- scanned_img.py
+++ scanned_img.py
@@ -16,11 +16,11 @@ def scanned_image(s):
 	g=grayscale(s)
 	i = 255-g
 	import scipy.ndimage
-	b = scipy.ndimage.filters.gaussian_filter(i,sigma=10)
+	b = scipy.ndimage.filters.gaussian_filter(i,sigma=20)
 	r= dodge(b,g)
 	return r
 
 
-import cv2
-img = cv2.imread("img.jpg")
-cv2.imwrite("result.jpg",scanned_image(img))
+
+
+
